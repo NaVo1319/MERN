d085488ef247cbaac497f7ede15343385e1ba889
Nazarkin V.I <vohammer2001@gmail.com>
2022-04-28 14:43:00 +0300

    The server

---
+++ server/index.js
@@ -0,0 +1,22 @@
+const express = require("express")
+const mongoose = require("mongoose")
+const config = require("config")
+const app = express()
+const PORT = config.get('serverPort')
+
+const authRouter = require("./routes/auth.routes")
+app.use(express.json())
+app.use("/api/auth", authRouter)
+
+
+const start =  async () =>{
+    try{
+        await mongoose.connect(config.get("dbUrl"))
+        app.listen(PORT,()=>{
+            console.log('Server started on port', PORT)
+        })
+    }catch (e){
+
+    }
+}
+start()
---
+++ server/models/User.js
@@ -0,0 +1,7 @@
+const {Schema, model, ObjectId} = require("mongoose")
+const User = new Schema({
+    email: {type: String, required: true, unique: true},
+    password: {type: String, required: true},
+    role: {type: String, required: true, default: "User"}
+})
+module.exports = model('User', User)
